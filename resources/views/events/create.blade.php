10158a6382d8a2ea0c42200e6a30874903229722
Alexandre <erdleosalgueiro@gmail.com>
2022-01-16 12:34:19 -0300

    Terceiro commit A

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,4 +6,13 @@
 
     <h1>Crie um evento</h1>
 
+    <div class="mb-3">
+        <label  class="form-label">Nome do Evento</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Descrição do Evento</label>
+        <textarea class="form-control" id="" rows="3"></textarea>
+      </div>
+
 @endsection
